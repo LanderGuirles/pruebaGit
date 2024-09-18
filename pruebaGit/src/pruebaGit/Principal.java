08b64bb16b15f037d4c71279e7fbb36e60e04482
Lander Guirles <lander.guirles@opendeusto.es>
2024-09-18 11:37:55 +0200

    Se termina la clase, otro mensaje

--- pruebaGit/src/pruebaGit/Principal.java
+++ pruebaGit/src/pruebaGit/Principal.java
@@ -2,6 +2,6 @@ package pruebaGit;
 
 public class Principal {
 		public static void main(String[] args) {
-			System.out.println("Adios mundo");
+			System.out.println("Se termina la clase");
 		}
 }
