68e5bfa37a7c6c21bcc91d3e84f32ec38aec8938
LanderGuirles <lander.guirles@opendeusto.es>
2024-09-23 08:25:38 +0200

    Update Principal.java

--- pruebaGit/src/pruebaGit/Principal.java
+++ pruebaGit/src/pruebaGit/Principal.java
@@ -2,6 +2,6 @@ package pruebaGit;
 
 public class Principal {
 		public static void main(String[] args) {
-			System.out.println("Estamos a lunes");
+			System.out.println("Estamos a lunes!!!!!");
 		}
 }
