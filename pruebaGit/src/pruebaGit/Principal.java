dc26b6252265b220b5153d6df49068aece299ba7
LanderGuirles <lander.guirles@opendeusto.es>
2024-09-23 08:12:38 +0200

    Update Principal.java
    primera clase

--- pruebaGit/src/pruebaGit/Principal.java
+++ pruebaGit/src/pruebaGit/Principal.java
@@ -2,6 +2,6 @@ package pruebaGit;
 
 public class Principal {
 		public static void main(String[] args) {
-			System.out.println("Se termina la clase");
+			System.out.println("Primera clase");
 		}
 }
